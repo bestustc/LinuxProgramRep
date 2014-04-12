6b7fb4e8495a5f72b69141d7c39227ba82b1c579
weijian2014 <303101610@qq.com>
2014-04-12 23:45:52 +0800

    remove Linux_Debug

--- Linux_Program/Linux_Debug/lib/threads/task.d
+++
@@ -1,6 +0,0 @@
-lib/threads/task.d: ../lib/threads/task.cpp ../lib/threads/task.h \
- ../lib/threads/../common/baseTypeDefine.h
-
-../lib/threads/task.h:
-
-../lib/threads/../common/baseTypeDefine.h:
